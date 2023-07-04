f1d4ec8f834124071d8ed00b4d0e199c41c242f5
Quick <quick0831@gmail.com>
2023-07-04 22:29:20 +0800

    Create `echo` command

--- src/main/java/edu/club/makereal/event_listener/CmdLineListener.java
+++ src/main/java/edu/club/makereal/event_listener/CmdLineListener.java
@@ -3,6 +3,8 @@ package edu.club.makereal.event_listener;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.Objects;
+
 public class CmdLineListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
@@ -12,5 +14,12 @@ public class CmdLineListener extends ListenerAdapter {
             return;
         String msg = event.getMessage().getContentRaw();
         event.getChannel().sendMessage(msg).submit();
+
+        String[] args = msg.split(" ");
+
+        if (args.length >= 2 && Objects.equals(args[0], "echo")) {
+            event.getJDA().getTextChannelById("1070643978438987786")
+                 .sendMessage(msg.split(" ", 2)[1]).submit();
+        }
     }
 }
