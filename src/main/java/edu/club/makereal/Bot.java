7a7dd8c3db01bc170aaad87c49ec8887b8f24977
Quick <quick0831@gmail.com>
2023-07-06 05:30:34 +0800

    Provide example code for async operation usage

--- src/main/java/edu/club/makereal/Bot.java
+++ src/main/java/edu/club/makereal/Bot.java
@@ -7,6 +7,7 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
 
 public class Bot {
 
@@ -18,6 +19,18 @@ public class Bot {
         HashMap<String, CmdCommand> cmd = new HashMap<>();
 
         cmd.put("echo", new CommandEcho());
+        cmd.put("sleep", (args_, event) -> CompletableFuture
+                .supplyAsync(() -> {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ignored) {
+                        return 2;
+                    }
+                    return 0;
+                })
+                .thenApplyAsync(res -> event.getChannel().sendMessage("sleep 1 second").submit())
+                .handle((result, exception) -> (exception == null) ? 0 : 1)
+        );
 
         JDABuilder.createDefault(args[0], intents)
                   .addEventListeners(new CmdLineListener(cmd))
